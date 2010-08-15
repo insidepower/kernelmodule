b52c86357328e58789b2762a9fe9349cdb9b64ae
insidepower <insidepower@greengrassl.(none)>
2010-08-16 00:11:31 +0800

    use module_init, module_exit, __init, __exit, __initdata

--- hello-1.c
+++ hello-1.c
@@ -1,17 +1,25 @@
 #include <linux/module.h>		/* needed by all modules */
 #include <linux/kernel.h>		/* printk: 8 level, DEFAULT_MESSAGE_LOGLEVEL */
+#include <linux/init.h>         /* for macro module_init(), module_exit() */
 
 /* info:
  * printk will appear on console if priority is less than "int console_loglevel" */
 
-int init_module (void)
+/* __initdata will ask kernel to free the data after init_module */
+static int data __initdata = 3;
+
+/* __init & __exit ask kernel to destroy this function once the functions finish */
+static int __init hello_3_init (void)
 {
 	printk(KERN_ALERT "Hello world 1\n");
 
 	return 0;
 }
 
-void cleanup_module(void)
+static int __exit hello3_exit (void)
 {
 	printk(KERN_ALERT "Bye!\n");
 }
+
+module_init(hello_3_init);
+module_exit(hello3_exit);
